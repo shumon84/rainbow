06a3a80e888c9e5ac14a8cc5f5f1573a5f4c7de8
shumon84 <cameremon84@gmail.com>
2019-03-26 09:18:53 +0900

    作業中

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"crypto/sha256"
+	"crypto/md5"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"runtime"
@@ -49,32 +50,6 @@ func CreateTable(H HashFunc, R ReductionFunc, t NumOfChains, m ChainLength, file
 	return file,nil
 }
 
-func main() {
-	// 第一引数がチェーン数
-	t, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	// 第二引数がチェーン長
-	m, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		log.Fatal(err)
-	}
-	fileName := fmt.Sprintf("rainbow_table_%d_%d.txt", t, m)
-	file,err := CreateTable(H, R, NumOfChains(t), ChainLength(m), fileName)
-	defer file.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for runtime.NumGoroutine() > 1 {
-		time.Sleep(time.Second / 2)
-	}
-	fmt.Println("ハッシュ関数 :",HTime)
-	fmt.Println("　　還元関数 :",RTime)
-	fmt.Println("順列生成関数 :",NextPermutationTime)
-}
-
 // ハッシュ関数
 var HTime = time.Duration(0)
 
@@ -83,7 +58,7 @@ func H(message []byte) []byte {
 	defer func() {
 		HTime += time.Since(startTime)
 	}()
-	digest := sha256.New()
+	digest := md5.New()
 	digest.Write(message)
 	return digest.Sum(nil)
 }
@@ -137,4 +112,102 @@ func nextPermutation(str string, index int) string {
 	// 現在の桁を0に戻す
 	str = str[:index] + MessageChars[:1] + str[index+1:]
 	return nextPermutation(str, index+1)
+}
+
+func main2() {
+	// 第一引数がチェーン数
+	t, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 第二引数がチェーン長
+	m, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileName := fmt.Sprintf("rainbow_table_%d_%d.txt", t, m)
+	file,err := CreateTable(H, R, NumOfChains(t), ChainLength(m), fileName)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for runtime.NumGoroutine() > 1 {
+		time.Sleep(time.Second / 2)
+	}
+	fmt.Println("ハッシュ関数 :",HTime)
+	fmt.Println("　　還元関数 :",RTime)
+	fmt.Println("順列生成関数 :",NextPermutationTime)
+}
+
+func ReHash(rainbow_table io.Reader,hash []byte)[]byte {
+	const numOfChain= 10000 // チェーン数
+	const chainLength= 3000 // チェーン長
+	//candidateList := [chainLength]string{}
+	candidateList := make(chan string, 4096)
+	//endList := [numOfChain]string{}
+
+	// チェーンの復元
+	fmt.Println("チェーン複合中")
+	numOfEndGoroutine :=0
+	for i := 0; i < chainLength; i++ {
+		go func(i int) {
+			rx := R(chainLength-i-1, hash)
+			for j := 0; j < i; j++ {
+				beforeHash := H(rx)
+				rx = R(chainLength-i+j, beforeHash)
+			}
+			candidateList <- string(rx)
+			numOfEndGoroutine++
+		}(i)
+	}
+	fmt.Println("チェーン複合終了")
+
+	// テーブルの読み込み
+	//fmt.Println("テーブル読み込み中")
+	//scanner := bufio.NewScanner(rainbow_table)
+	//for i := 0; scanner.Scan(); i++ {
+	//	line := scanner.Text()
+	//	endList[i] = line[9:]
+	//	fmt.Println(i,endList[i])
+	//}
+	//fmt.Println("テーブル読み込み終了")
+
+	fmt.Println("待ち合わせ中")
+	for numOfEndGoroutine != chainLength {}
+	fmt.Println("待ち合わせ終了")
+
+	// テーブルとチェーンの照合
+	fmt.Println("テーブルとチェーンの照合中")
+	answer := ""
+	//for i := 0; i < numOfChain; i++ {
+	//	for j := 0; j < chainLength; j++ {
+	//		if RainbowTable[i] == candidateList[j] {
+	//			answer = candidateList[j]
+	//		}
+	//	}
+	//}
+	for candidate := range candidateList{
+		go func(candidate string){
+			for _,rainbow := range RainbowTable{
+				if rainbow == candidate{
+					fmt.Println(candidate)
+				}
+			}
+		}(candidate)
+	}
+
+	return []byte(answer)
+}
+
+func main() {
+	//fileName := os.Args[1]
+	//file,err := os.Open(fileName)
+	//if err != nil{
+	//	log.Fatal(err)
+	//}
+	hash := H([]byte("90000000"))
+
+	plain := ReHash(nil,hash)
+	fmt.Println(string(plain))
 }
